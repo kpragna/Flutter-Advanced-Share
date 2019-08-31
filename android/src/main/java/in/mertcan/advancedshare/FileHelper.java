2c5ea96f600d487921674bcd7f3bf93c0e44f92f
kpragna <pragnak@purnatva.com>
2019-08-31 11:38:01 +0530

    Update FileHelper.java

--- android/src/main/java/in/mertcan/advancedshare/FileHelper.java
+++ android/src/main/java/in/mertcan/advancedshare/FileHelper.java
@@ -6,9 +6,10 @@ import android.content.CursorLoader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.util.Base64;
 import android.webkit.MimeTypeMap;
+
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.FileOutputStream;
